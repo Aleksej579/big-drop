ff4e08d65e786935a08e4634a9ae7c198f7fd74f
Aleksej579 <aleksej.eremenko579@gmail.com>
2021-03-02 01:01:30 +0200

    end complete project. next link and mediarequest

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// slider from swiper
 var swiper = new Swiper('.swiper-container', {
     effect: 'fade',
     autoplay: {
@@ -5,3 +6,96 @@ var swiper = new Swiper('.swiper-container', {
         disableOnInteraction: false,
     },
 });
+
+// load content for menu (start)
+$.ajax({
+    url: './template/page-1.html',
+    type: 'GET',
+    beforeSend: function () {
+        $('#content').empty();
+    },
+    success: function (responce) {
+        $('#content').append(responce);
+    },
+    error: function () {
+        alert('Error!');
+    }
+});
+
+// load content for menu 
+$('#about, .about, .wrapper_logo').on('click', function () {
+    $.ajax({
+        url: './template/page-1.html',
+        type: 'GET',
+        beforeSend: function () {
+            $('#content').empty();
+        },
+        success: function (responce) {
+            $('#content').append(responce);
+        },
+        error: function () {
+            alert('Error!');
+        }
+    });
+});
+$('#guides, .guides').on('click', function () {
+    $.ajax({
+        url: `./template/page-2.html`,
+        type: 'GET',
+        beforeSend: function () {
+            $('#content').empty();
+        },
+        success: function (responce) {
+            $('#content').append(responce);
+        },
+        error: function () {
+            alert('Error!');
+        }
+    });
+});
+$('#community, .community').on('click', function () {
+    $.ajax({
+        url: `./template/page-3.html`,
+        type: 'GET',
+        beforeSend: function () {
+            $('#content').empty();
+        },
+        success: function (responce) {
+            $('#content').append(responce);
+        },
+        error: function () {
+            alert('Error!');
+        }
+    });
+});
+$('#resources, .resources').on('click', function () {
+    $.ajax({
+        url: `./template/page-4.html`,
+        type: 'GET',
+        beforeSend: function () {
+            $('#content').empty();
+        },
+        success: function (responce) {
+            $('#content').append(responce);
+        },
+        error: function () {
+            alert('Error!');
+        }
+    });
+});
+$('#contact_us, .contact_us').on('click', function () {
+    $.ajax({
+        url: `./template/page-5.html`,
+        type: 'GET',
+        beforeSend: function () {
+            $('#content').empty();
+        },
+        success: function (responce) {
+            $('#content').append(responce);
+        },
+        error: function () {
+            alert('Error!');
+        }
+    });
+});
+
